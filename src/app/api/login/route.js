018189368e815af3d0944d1543c5f2644ccbf188
Amey <ameygaikar28@gmail.com>
2024-03-31 19:41:15 +0530

    add dbFunctions

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,10 +1,28 @@
+import { CreateNewStudent, CreateNewTranaction } from "@/app/dbConfig/dbConfig"
 import { NextResponse } from "next/server"
 
 export async function POST(req) {
   try {
     const reqBody = await req.json() 
-    const { name, prnNo, gender, caste, branch, year, amount, transactionID } = reqBody
+    const { name, prnNo, gender, category, branch, year, amount, transactionID } = reqBody
     console.log(reqBody)
+    CreateNewStudent({
+      data: {
+        name: name,
+        prnNo: prnNo,
+        branch: branch,
+        year: year,
+        gender: gender,
+        category: category,
+      },
+    })
+    CreateNewTranaction({
+      data: {
+        transactionID: transactionID,
+        rollNo: prnNo,
+        amount: amount,
+      },
+    })
     return NextResponse.json({message: "Successful", status: 200})
   } catch(error) {
     console.log(error)
