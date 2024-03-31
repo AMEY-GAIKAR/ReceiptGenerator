3661673ea4388742ca303bf50540125f764fa249
Amey <ameygaikar28@gmail.com>
2024-04-01 02:33:23 +0530

    make db call

--- src/app/view/page.jsx
+++ src/app/view/page.jsx
@@ -3,11 +3,25 @@
 import { useSearchParams } from "next/navigation"
 import { Template } from "../ui/pdf-template" 
 
-export default function View() {
+export default async function View() {
   const searchParams = useSearchParams()
   const result = searchParams.get('student')
   console.log(result)
   const student = JSON.parse(result)
+  try {
+    const response = await fetch("/api/login", {
+      method: "POST",
+      body: JSON.stringify(student),
+      headers: {
+        'content-type': 'application/json'
+      },
+    })
+    console.log("Submission successful", response)
+    } catch (error) {
+    console.log("Submission failed", error)
+    } finally {
+    
+    }
 
   return (
     <Template student={student} />
